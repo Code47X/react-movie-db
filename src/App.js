ed352339581289f8033210a19210f2b7d00c2f2f
Tanner Hartwig <tanner.hartwig57@gmail.com>
2018-02-05 20:13:42 -0600

    Messing around with states

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    toggle: true
+  }
+
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <MyFirstComponent text="Welcome to React!" />
+          <MyFirstComponent text="Welcome to React!" toggle={this.state.toggle} />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {this.state.toggle && 
+          <p>This should show and hide</p>
+        }
+        <button onClick={this.toggle}>Show / Hide</button>
       </div>
     );
   }
@@ -20,8 +35,8 @@ class App extends Component {
 
 class MyFirstComponent extends Component {
   render() {
-    const { text } = this.props;
-
+    const { text, toggle } = this.props;
+    console.log(toggle)
     return (
       <h1 className="App-title">{text}</h1>
     );
