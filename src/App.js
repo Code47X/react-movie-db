788a3a6574242249dd6cb1802993c1fd12d74c7d
Tanner Hartwig <tanner.hartwig57@gmail.com>
2018-02-05 19:32:20 -0600

    My first component :)

--- src/App.js
+++ src/App.js
@@ -8,12 +8,20 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <MyFirstComponent />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+    );
+  }
+}
+
+class MyFirstComponent extends Component {
+  render() {
+    return (
+      <h1 className="App-title">Welcome to React</h1>
     );
   }
 }
